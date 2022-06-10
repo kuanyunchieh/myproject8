712c0a8e6d564d594ad802993b342d38661c0699
amandakuan <amandakuan6@gmail.com>
2022-06-10 12:55:57 +0800

    Kotlin hello

---
+++ src/main/java/PersonKotlin.kt
@@ -0,0 +1,13 @@
+package com.kuan
+
+class PersonKotlin {
+    fun greeting(){
+        println("Hello Kotlin")
+    }
+}
+
+//first level
+fun main() {
+    val p = PersonKotlin()
+    p.greeting()
+}
